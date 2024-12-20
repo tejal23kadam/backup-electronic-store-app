cdc7a49de00ed332228ee703f7ce27f77774bd38
tejal23kadam <tejalmchandlekar@gmail.com>
2024-12-20 12:34:54 +0530

    add commit

--- src/component/singleProductDetailComponent/SingleProductDetailPageNew.js
+++ src/component/singleProductDetailComponent/SingleProductDetailPageNew.js
@@ -28,24 +28,13 @@ function SingleProductDetailPageNew() {
                                                 </div>
 
                                                 {/* <!-- product slider --> */}
-					<div class="product-slider">
-						<div class="product-slider-item my-4" data-image="images/products/products-1.jpg">
-							<img class="img-fluid w-100" src="images/products/products-1.jpg" alt="product-img"/>
-						</div>
-						<div class="product-slider-item my-4" data-image="images/products/products-2.jpg">
-							<img class="d-block img-fluid w-100" src="images/products/products-2.jpg" alt="Second slide"/>
-						</div>
-						<div class="product-slider-item my-4" data-image="images/products/products-3.jpg">
-							<img class="d-block img-fluid w-100" src="images/products/products-3.jpg" alt="Third slide"/>
-						</div>
-						<div class="product-slider-item my-4" data-image="images/products/products-1.jpg">
-							<img class="d-block img-fluid w-100" src="images/products/products-1.jpg" alt="Third slide"/>
-						</div>
-						<div class="product-slider-item my-4" data-image="images/products/products-2.jpg">
-							<img class="d-block img-fluid w-100" src="images/products/products-2.jpg" alt="Third slide"/>
-						</div>
-					</div>
-					{/* <!-- product slider --> */}
+                                                <div class="product-slider">
+                                                    <div class="product-slider-item my-4" data-image="images/products/products-1.jpg">
+                                                        <img class="img-fluid w-100" src="images/products/products-1.jpg" alt="product-img" />
+                                                    </div>
+
+                                                </div>
+                                                {/* <!-- product slider --> */}
 
                                                 <div class="content mt-5 pt-5">
                                                     <ul class="nav nav-pills  justify-content-center" id="pills-tab" role="tablist">
